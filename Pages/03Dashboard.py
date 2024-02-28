fe8ae4bfe274686f7001e3d5e4fe9de47f6f87b2
Angella Nakkungu <angella.nakkungu@outlook.com>
2024-02-28 17:19:09 +0300

    Predict Page

--- Pages/03Dashboard.py
+++ Pages/03Dashboard.py
@@ -1,136 +1,125 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import plotly.express as px
-import plotly.figure_factory as ff
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from joblib import load
+from streamlit_extras.switch_page_button import switch_page
 
-from joblib import dump, load
-# Load the cleaned data using joblib
-loaded_cleaned_training_data = load('Packages\cleaned_training_data.joblib')
-
-# Display the loaded data using Streamlit
-st.title('Visualizations from cleaned data')
-
-# Display the loaded data using Streamlit
-st.header("Loaded Cleaned Training Data Sample:")
-st.dataframe(loaded_cleaned_training_data.head())
-
-# Separate categorical and numerical columns
-categorical_columns = loaded_cleaned_training_data.select_dtypes(include=['object']).columns
-numerical_columns = loaded_cleaned_training_data.select_dtypes(exclude=['object']).columns
-
-# Plot histograms for numerical variables
-st.header("Histograms for Numerical Variables")
-for col in numerical_columns:
-    fig = px.histogram(loaded_cleaned_training_data, x=loaded_cleaned_training_data[col], title=f'Histogram for {col}')
-    st.plotly_chart(fig)
-
-# Plot box plots 
-#monthly charges
-boxplot_trace = go.Box(y=loaded_cleaned_training_data['MonthlyCharges'], name='MonthlyCharges', marker=dict(color='rgba(150, 10, 50, 0.6)'))
-
-# Create layout for the plot
-layout = go.Layout(
-    title='Box Plot of MonthlyCharges',
-    yaxis=dict(title='MonthlyCharges'),
-    template='plotly_white'
+st.set_page_config(
+    page_title='My Awesome App',
+    page_icon='🏠',
+    layout='wide'
 )
+if st.session_state.get("authentication_status"):
+    if st.session_state["authentication_status"]:
 
-# Create figure object and plot
-boxplot_mc = go.Figure(data=[boxplot_trace], layout=layout)
-
-# Display the plot in Streamlit
-st.plotly_chart(boxplot_mc)
-
-#tenure
-boxplot_trace = go.Box(y=loaded_cleaned_training_data['tenure'], name='tenure', marker=dict(color='rgba(150, 10, 50, 0.6)'))
-
-# Create layout for the plot
-layout = go.Layout(
-    title='Box Plot of Tenure',
-    yaxis=dict(title='Tenure'),
-    template='plotly_white'
-)
-
-# Create figure object and plot
-boxplot_mc = go.Figure(data=[boxplot_trace], layout=layout)
-
-# Display the plot in Streamlit
-st.plotly_chart(boxplot_mc)
-
-#total charges
-boxplot_trace = go.Box(y=loaded_cleaned_training_data['TotalCharges'], name='TotalCharges', marker=dict(color='rgba(150, 10, 50, 0.6)'))
-
-# Create layout for the plot
-layout = go.Layout(
-    title='Box Plot of TotalCharges',
-    yaxis=dict(title='TotalCharges'),
-    template='plotly_white'
-)
-
-# Create figure object and plot
-boxplot_mc = go.Figure(data=[boxplot_trace], layout=layout)
-
-# Display the plot in Streamlit
-st.plotly_chart(boxplot_mc)
+        # Display top navigation buttons
+        col1, col2, col3, col4, col5 = st.columns(5)
+        if col1.button("Home"):
+            switch_page("Home")
+        if col2.button("Data"):
+            switch_page("Data")
+        if col3.button("Dashboard"):
+            switch_page("Dashboard")
+        if col4.button("Predict"):
+            switch_page("Predict")
+        if col5.button("History"):
+            switch_page("History")
 
 
-# Plot heatmap for numerical variables
-# Calculate correlation matrix
-corr = loaded_cleaned_training_data[numerical_columns].corr()
+        # Load the cleaned data using joblib
+        loaded_cleaned_training_data = load('Packages/cleaned_training_data.joblib')
 
-# Define custom color scale
-colors = [
-    (0.0, "blue"),
-    (0.5, "white"),
-    (1.0, "red")
-]
+        # Display the loaded data using Streamlit
+        st.title('Visualizations from cleaned data')
+        st.header("Loaded Cleaned Training Data Sample:")
+        st.dataframe(loaded_cleaned_training_data.head())
 
-# Plot heatmap with custom color scale
-fig = ff.create_annotated_heatmap(z=corr.values,
-                                  x=list(corr.columns),
-                                  y=list(corr.index),
-                                  colorscale=colors,
-                                  showscale=True)
-fig.update_layout(title_text='Correlation Heatmap of Numerical Variables',
-                  xaxis=dict(title='Numerical Variables'),
-                  yaxis=dict(title='Numerical Variables'))
-st.plotly_chart(fig)
+        # EDA selection
+        st.sidebar.title('Exploratory Data Analysis (EDA)')
 
-# Plot pairplots for numerical variables
-st.header("Pairplots for Numerical Variables")
-pairplot_fig = px.scatter_matrix(loaded_cleaned_training_data[numerical_columns],
-                                 height=800, width=800)  # Adjust height and width
-st.plotly_chart(pairplot_fig)
+        # List of EDA options
+        eda_options = ['Visualizations', 'KPIs']
+
+        # Allow user to select EDA options
+        selected_eda_option = st.sidebar.selectbox('Select EDA Option', eda_options)
+
+        if selected_eda_option == 'Visualizations':
+
+            # Separate categorical and numerical columns
+            categorical_columns = loaded_cleaned_training_data.select_dtypes(include=['object']).columns
+            numerical_columns = loaded_cleaned_training_data.select_dtypes(exclude=['object']).columns
+
+            # Plot histograms for numerical variables
+            st.header("Histograms for Numerical Variables")
+            hist_cols = st.columns(len(numerical_columns))
+            for col, numerical_column in zip(hist_cols, numerical_columns):
+                fig = px.histogram(loaded_cleaned_training_data, x=loaded_cleaned_training_data[numerical_column], title=f'Histogram for {numerical_column}')
+                col.plotly_chart(fig)
+
+            # Plot box plots for numerical variables
+            st.header("Box Plots for Numerical Variables")
+            boxplot_cols = st.columns(len(numerical_columns))
+            for col, numerical_column in zip(boxplot_cols, numerical_columns):
+                boxplot_trace = go.Box(y=loaded_cleaned_training_data[numerical_column], name=numerical_column, marker=dict(color='rgba(150, 10, 50, 0.6)'))
+                layout = go.Layout(title=f'Box Plot of {numerical_column}', yaxis=dict(title=numerical_column), template='plotly_white')
+                boxplot_fig = go.Figure(data=[boxplot_trace], layout=layout)
+                col.plotly_chart(boxplot_fig)
+
+            # Plot heatmap for numerical variables
+            st.header("Correlation Heatmap of Numerical Variables")
+            corr = loaded_cleaned_training_data[numerical_columns].corr()
+            fig = ff.create_annotated_heatmap(z=corr.values, x=list(corr.columns), y=list(corr.index), colorscale='Viridis')
+            fig.update_layout(xaxis_title='Numerical Variables', yaxis_title='Numerical Variables')
+            st.plotly_chart(fig)
+
+            # Plot pairplots for numerical variables
+            st.header("Pairplots for Numerical Variables")
+            pairplot_fig = px.scatter_matrix(loaded_cleaned_training_data[numerical_columns], height=800, width=800)
+            st.plotly_chart(pairplot_fig)
+
+            # Plot bar plots for categorical variables
+            st.title('Categorical Variable Analysis')
+            for feature in categorical_columns:
+                description = loaded_cleaned_training_data[feature].describe()
+                unique_values_counts = loaded_cleaned_training_data[feature].value_counts()
+                missing_values_count = loaded_cleaned_training_data[feature].isnull().sum()
+                missing_values_percentage = (missing_values_count / len(loaded_cleaned_training_data)) * 100
+                
+                fig = go.Figure(go.Bar(x=unique_values_counts.index, y=unique_values_counts.values))
+                fig.update_layout(title=f"Distribution of {feature}", xaxis_title=feature, yaxis_title="Count")
+                
+                st.header(f"Univariate Analysis of Feature: {feature}")
+                st.write(description)
+                st.plotly_chart(fig)
+
+        elif selected_eda_option == 'KPIs':
+            # Calculate average tenure per customer segment
+            avg_tenure_per_segment = loaded_cleaned_training_data.groupby('Churn')['tenure'].mean()
+
+            # Calculate average monthly charge per customer segment
+            avg_monthly_charge_per_segment = loaded_cleaned_training_data.groupby('Churn')['MonthlyCharges'].mean()
+
+            # Convert 'Yes' and 'No' to 1 and 0 respectively
+            loaded_cleaned_training_data['Churn'] = loaded_cleaned_training_data['Churn'].map({'Yes': 1, 'No': 0})
+
+            # Calculate churn rate
+            churn_rate = loaded_cleaned_training_data['Churn'].mean()
+            # Display churn rate with bigger font size
 
 
-#categorical variables
-# Create an empty list to store Plotly figures
-figures = []
-# Loop through each categorical variable
-for feature in categorical_columns:
-    # Describe the feature
-    description = loaded_cleaned_training_data[feature].describe()
-    unique_values_counts = loaded_cleaned_training_data[feature].value_counts()
-    missing_values_count = loaded_cleaned_training_data[feature].isnull().sum()
-    missing_values_percentage = (missing_values_count / len(loaded_cleaned_training_data)) * 100
-    
-    # Create a bar plot for the distribution of the variable
-    fig = go.Figure(go.Bar(x=unique_values_counts.index, y=unique_values_counts.values))
-    fig.update_layout(title=f"Distribution of {feature}",
-                      xaxis_title=feature,
-                      yaxis_title="Count")
-    
-    # Add figure to the list
-    figures.append((description, fig))
+            st.header('Key Performance Indicators (KPIs)')
+            st.subheader('Average Tenure Per Customer Segment')
+            st.write(avg_tenure_per_segment)
 
-# Display figures in Streamlit app
-st.title('Categorical Variable Analysis')
+            st.subheader('Average Monthly Charge Per Customer Segment')
+            st.write(avg_monthly_charge_per_segment)
 
-# Display each figure
-for description, fig in figures:
-    st.header(f"Univariate Analysis of Feature: {description.name}")
-    st.write(description)
-    st.plotly_chart(fig)
+            st.header('Churn Rate')
+            st.write(f"<h3>{churn_rate}</h3>", unsafe_allow_html=True)
+            st.write("A churn rate of 0.26497421658072196 means that approximately 26.5 percent of customers are churning.")
+    else:
+        st.error('Username/password is incorrect')
+else:
+    st.error('Please log in to access this page.')           
--- Pages/04predict.py
+++ Pages/04predict.py
@@ -1,47 +1,157 @@
 import streamlit as st
+import joblib
+import pandas as pd
+from streamlit_extras.switch_page_button import switch_page
 
-st.set_page_config(
-    page_title='Predict',
-    page_icon='🔮',
-    layout='wide'
-)
-# #['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
-#        'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
-#        'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
-#        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
-#        'MonthlyCharges', 'TotalCharges', 'Churn']
-
-def form1():
-
-    col1, col2 = st.columns(2)
-
-    with st.form('Insert-Variable-Values'):
-        with col1:
-            st.subheader('Numerical Variables')
-            st.number_input('Enter Total Charge')
-            st.number_input('Enter Monthly Charge')
-            st.number_input('Enter Tenure')
-        with col2:
-            st.subheader('Categorical Variables')
-            st.selectbox('Enter gender', options=['Female', 'Male'])
-            st.selectbox('Enter Senior Citizenship status', options=['Yes', 'No'])
-            st.selectbox('Enter Dependants status', options=['Yes', 'No'])
-            st.selectbox('Enter Phone Service status', options=['Yes', 'No'])
-            st.selectbox('Enter Multiple Lines status', options=['None', 'No', 'Yes', 'No phone service'])
-            st.selectbox('Enter InternetService status', options=['DSL', 'Fiber optic', 'No'])
-            st.selectbox('Enter Online Security status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Online BackUp status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Device Protection status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Tech Support status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Streming TV status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Streaming Movies status', options=['No', 'Yes',' None', 'No internet service'])
-            st.selectbox('Enter Contract status', options=['Month-to-month', 'One year', 'Two year'])
-            st.selectbox('Enter Paperless billing status', options=['Yes', 'No'])
-            st.selectbox('Enter Payment Method', options=['Electronic check', 'Mailed check', 'Bank transfer(automatic)','Credit Card(automatic)'])
+if st.session_state.get("authentication_status"):
+    if st.session_state["authentication_status"]:
 
 
+        col1, col2, col3, col4, col5 = st.columns(5)
 
-        st.form_submit_button('Submit')
+        if col1.button("Home"):
+            switch_page("Home")
+
+        if col2.button("data"):
+            switch_page("data")
+
+        if col3.button("dashboard"):
+            switch_page("dashboard")
+
+        if col4.button("predict"):
+            switch_page("predict")
+
+        if col5.button("history"):
+            switch_page("history")
+        # st.set_page_config(
+        #     page_title='Predict',
+        #     page_icon='🔮',
+        #     layout='wide'
+        # )
+        # #['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
+        #        'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
+        #        'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+        #        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
+        #        'MonthlyCharges', 'TotalCharges', 'Churn']
+        st.cache_resource(show_spinner ='Loading')
+
+        def load_model1():
+            pipeline = joblib.load('Models\decision_tree_model.joblib')
+            return pipeline
 
 
-form1()
+        st.cache_resource(show_spinner ='Loading')
+        def load_model2():
+            pipeline = joblib.load('Models\logistic_regression_model.joblib')
+            return pipeline
+
+        st.cache_resource()
+
+        def select_model():
+            encoder = joblib.load('Models\label_encoder.joblib')
+
+            st.selectbox('Select a model', options=['Decision Tree', 'Random Forest'],
+            key = 'selected_model')
+
+            if st.session_state['selected_model'] == 'Random Forest':
+                pipeline=load_model2()
+
+            else:
+                pipeline = load_model1()
+
+            return pipeline, encoder
+
+        def make_prediction(pipeline, encoder):
+
+            Total_Charge = st.session_state['Total_Charge']
+            Monthly_Charge = st.session_state['Monthly_Charge']
+            Tenure = st.session_state['Tenure']
+            gender = st.session_state['gender']
+            Senior_Citizenship = st.session_state['Senior_Citizenship']
+            Dependants = st.session_state['Dependants']
+            Phone_Service = st.session_state['Phone_Service']
+            Multiple_Lines = st.session_state['Multiple_Lines']
+            InternetService = st.session_state['InternetService']
+            Online_Security = st.session_state['Online_Security']
+            Online_BackUp = st.session_state['Online_BackUp']
+            Device_Protection = st.session_state['Device_Protection']
+            Tech_Support = st.session_state['Tech_Support']
+            Streaming_TV= st.session_state['Streming_TV']
+            Streaming_Movies= st.session_state['Streaming_Movies']
+            Contract = st.session_state['Contract']
+            Paperless_billing = st.session_state['Paperless_billing']
+            Payment_Method= st.session_state['Payment_Method']
+            gender = st.session_state['gender']
+            
+
+            columns = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
+            'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
+                'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+            'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
+            'MonthlyCharges', 'TotalCharges']
+            
+            data = [[Total_Charge, Monthly_Charge,Tenure, gender,Senior_Citizenship,  Dependants, Phone_Service,
+                    Multiple_Lines,InternetService,Online_Security, Online_BackUp, Device_Protection,Tech_Support,
+                        Streaming_TV, Streaming_Movies, Contract,Paperless_billing,  Payment_Method ]]
+
+            df = pd.DataFrame(data,columns=columns)
+
+            pred = pipeline.predict(df)
+
+            st.session_state['pred'] = pred
+
+            return pred
+        def form1():
+            with st.form('Insert-Variable-Values'):
+            
+                # pipeline, encoder = select_model()
+                
+                col1, col2, col3 = st.columns(3)
+
+            
+                with col1:
+                    st.subheader('Numerical Variables')
+                    st.number_input('Enter Total Charge', key = 'Total_Charge')
+                    st.number_input('Enter Monthly Charge', key = 'Monthly_Charge')
+                    st.number_input('Enter Tenure',key ='Tenure')
+                with col2:
+                    st.subheader('Categorical Variables')
+                    st.selectbox('Enter gender', options=['Female', 'Male'], key='gender')
+                    st.selectbox('Enter Senior Citizenship status', options=['Yes', 'No'], key= 'Senior_Citizenship')
+                    st.selectbox('Enter Dependants status', options=['Yes', 'No'], key = 'Dependants')
+                    st.selectbox('Enter Phone Service status', options=['Yes', 'No'], key ='Phone_Service')
+                    st.selectbox('Enter Multiple Lines status', options=['None', 'No', 'Yes', 'No phone service'], key='Multiple_Lines' )
+                    st.selectbox('Enter InternetService status', options=['DSL', 'Fiber optic', 'No'], key ='InternetService')
+                    st.selectbox('Enter Online Security status', options=['No', 'Yes',' None', 'No internet service'], key ='Online_Security')
+                    st.selectbox('Enter Online BackUp status', options=['No', 'Yes',' None', 'No internet service'], key ='Online_BackUp')
+
+                with col3:
+                    st.subheader('Categorical Variables')
+                    st.selectbox('Enter Device Protection status', options=['No', 'Yes',' None', 'No internet service'], key = 'Device_Protection')
+                    st.selectbox('Enter Tech Support status', options=['No', 'Yes',' None', 'No internet service'], key ='Tech_Support')
+                    st.selectbox('Enter Streaming TV status', options=['No', 'Yes',' None', 'No internet service'], key ='Streaming_TV')
+                    st.selectbox('Enter Streaming Movies status', options=['No', 'Yes',' None', 'No internet service'], key ='Streaming_Movies')
+                    st.selectbox('Enter Contract status', options=['Month-to-month', 'One year', 'Two year'], key ='Contract')
+                    st.selectbox('Enter Paperless billing status', options=['Yes', 'No'], key ='Paperless_billing')
+                    st.selectbox('Enter Payment Method', options=['Electronic check', 'Mailed check', 'Bank transfer(automatic)','Credit Card(automatic)'], key ='Payment_Method')
+            # st.form_submit_button('Submit', on_click=make_prediction, kwargs=dict(pipeline=pipeline, encoder=encoder))
+                    
+                st.form_submit_button('Submit')
+
+
+            # select_model()
+            
+        if __name__ == '__main__':
+            st.title('Prediction')
+            form1()
+            
+            
+                
+
+            # final_pred = st.session_state['pred']
+            # st.write(final_pred)
+            st.write(st.session_state)
+    else:
+        st.error('Username/password is incorrect')
+else:
+    st.error('Please log in to access this page.')  
